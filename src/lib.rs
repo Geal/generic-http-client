0c20da0a59cb190da9f3b3901980723dc111028f
Geoffroy Couprie <contact@geoffroycouprie.com>
2020-08-11 15:33:00 +0200

    parse the body

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,44 @@
-use std::io::{self, Read, Write, BufWriter, BufRead, ErrorKind};
 use std::fmt::Debug;
+use std::io::{self, BufRead, BufReader, BufWriter, Read, Write};
 use std::marker::PhantomData;
 
-pub struct Client<Stream: Read+Write, R: Resolver<Stream>> {
+pub struct Client<Stream: Read + Write, R: Resolver<Stream>> {
     //resolver: R,
     stream: Option<Stream>,
     resolver: PhantomData<R>,
 }
 
-impl<Stream: Read+Write+Debug, R: Resolver<Stream>> Client<Stream, R> {
-    pub fn new(hostname: &str) -> Result<Self, HttpError> {
-        let stream = Some(R::resolve(hostname).map_err(HttpError::Resolver)?);
+impl<Stream: Read + Write + Debug, R: Resolver<Stream>> Client<Stream, R> {
+    pub fn new(url: &str) -> Result<Self, HttpError> {
+        let stream = Some(R::resolve(url)?);
 
-        Ok(Client { stream, resolver: PhantomData })
+        Ok(Client {
+            stream,
+            resolver: PhantomData,
+        })
     }
 
     pub fn get(url_str: &str) -> Result<http::Response<Body<Stream>>, HttpError> {
-        let url = url::Url::parse(url_str).map_err(HttpError::Url)?;
-
-        let host = match url.port() {
-            Some(p) => format!("{}:{}", url.host_str().unwrap(), p),
-            None => url.host_str().unwrap().to_string(),
-        };
-
-        let mut client: Self = Client::new(&host)?;
+        let mut client: Self = Client::new(&url_str)?;
         let mut req = http::Request::get(url_str).body(&b""[..]).unwrap();
         client.send(req)?;
         let response = client.receive()?;
         Ok(response)
     }
 
-    pub fn send<T: BufRead+HasLength>(&mut self, mut req: http::Request<T>) -> Result<(), HttpError> {
+    pub fn send<T: BufRead + HasLength>(
+        &mut self,
+        mut req: http::Request<T>,
+    ) -> Result<(), HttpError> {
         let mut stream = BufWriter::new(self.stream.take().unwrap());
 
         // we'are assuming that the reqest line and all headers will fit into the buffer
-        write!(&mut stream, "{} {} HTTP/1.1\r\n", req.method().as_str(), req.uri().to_string())?;
+        write!(
+            &mut stream,
+            "{} {} HTTP/1.1\r\n",
+            req.method().as_str(),
+            req.uri().to_string()
+        )?;
 
         for (name, value) in req.headers() {
             write!(&mut stream, "{}: ", name.as_str())?;
@@ -80,27 +84,19 @@ impl<Stream: Read+Write+Debug, R: Resolver<Stream>> Client<Stream, R> {
     }
 
     fn receive(&mut self) -> Result<http::Response<Body<Stream>>, HttpError> {
-        let mut buffer: Vec<u8> = std::iter::repeat(0).take(16384).collect();
-
         let mut response = http::Response::builder();
-        let mut stream = self.stream.take().unwrap();
+        let mut stream = BufReader::new(self.stream.take().unwrap());
         let mut index = 0usize;
         let mut at_eof = false;
-        let parsed_length = loop {
-            if index == buffer.len() {
-                panic!("the buffer was too small to parse the response");
-            }
 
-            let sz = stream.read(&mut buffer[index..])?;
-            index += sz;
-
-            let at_eof = sz == 0;
-
+        loop {
+            let data = stream.fill_buf()?;
+            let at_eof = data.len() == 0;
 
             let mut headers = [httparse::EMPTY_HEADER; 30];
             let mut res = httparse::Response::new(&mut headers);
 
-            let status = res.parse(&buffer[..index])?;
+            let status = res.parse(stream.buffer())?;
             if status.is_partial() {
                 if at_eof {
                     panic!("got partial response and EOF");
@@ -117,11 +113,9 @@ impl<Stream: Read+Write+Debug, R: Resolver<Stream>> Client<Stream, R> {
                 response = response.header(header.name, std::str::from_utf8(header.value).unwrap());
             }
 
-            break parsed_length;
-        };
-
-        buffer = buffer.split_off(parsed_length);
-        println!("parsed {} bytes, got response:\n{:?}", parsed_length, response);
+            stream.consume(parsed_length);
+            break;
+        }
 
         let mut length = Length::None;
         if let Some(headers) = response.headers_ref() {
@@ -137,19 +131,16 @@ impl<Stream: Read+Write+Debug, R: Resolver<Stream>> Client<Stream, R> {
                 for mut element in elements {
                     let s = element.trim().to_lowercase();
                     if &s == "chunked" {
-                        length = Length::Chunked;
+                        length = Length::Chunked(0);
                     }
                 }
-
             }
-
         }
 
         let body = Body {
-          stream,
-          length,
-          buffer,
-          at_eof,
+            stream,
+            length,
+            at_eof,
         };
 
         Ok(response.body(body)?)
@@ -157,51 +148,113 @@ impl<Stream: Read+Write+Debug, R: Resolver<Stream>> Client<Stream, R> {
 }
 
 #[derive(Debug)]
-pub struct Body<Stream: Read+Debug> {
-    stream: Stream,
+pub struct Body<Stream: Read + Debug> {
+    stream: BufReader<Stream>,
     length: Length,
-    buffer: Vec<u8>,
     at_eof: bool,
 }
 
 #[derive(Debug)]
 pub enum Length {
     None,
+    //remaining size
     ContentLength(usize),
-    Chunked,
+    // remaining size in the current chunk
+    Chunked(usize),
 }
 
-impl<Stream: Read+Debug> Body {
-    fn fill(&mut self) -> io::Result<usize> {
-
-
-    }
-}
-
-
-/*
-impl<Stream: Read+Debug> Read for Body<Stream> {
+impl<Stream: Read + Debug> Read for Body<Stream> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let (length, res) = match self.length {
             Length::None => return Ok(0),
             Length::ContentLength(sz) => {
-                let bound = std::cmp::min(sz, buf.len());
-
-                let mut index = 0usize;
-
-                loop {
-
-
-
-
+                if sz == 0 {
+                    return Ok(0);
                 }
 
+                if self.stream.buffer().len() == 0 {
+                    if self.at_eof {
+                        return Ok(0);
+                    } else {
+                        let data = self.stream.fill_buf()?;
 
+                        if data.is_empty() {
+                            self.at_eof = true;
+                            return Ok(0);
+                        }
+                    }
+                }
 
-            },
-            Length::Chunked => {
+                let bound = std::cmp::min(sz, buf.len());
+                let internal_bound = std::cmp::min(bound, self.stream.buffer().len());
 
-            },
+                let written = (&mut buf[..bound]).write(&self.stream.buffer()[..internal_bound])?;
+                self.stream.consume(written);
+                (Length::ContentLength(sz - written), Ok(written))
+            }
+            Length::Chunked(mut sz) => {
+                if sz == 0 {
+                    if self.stream.buffer().is_empty() {
+                        if self.at_eof {
+                            return Ok(0);
+                        }
+
+                        let data = self.stream.fill_buf()?;
+
+                        if data.is_empty() {
+                            self.at_eof = true;
+                            return Ok(0);
+                        }
+                    }
+
+                    let (parsed, chunk_size) = loop {
+                        match httparse::parse_chunk_size(self.stream.buffer()) {
+                            Err(invalid_chunk_size) => {
+                                return Err(io::Error::new(
+                                    io::ErrorKind::Other,
+                                    "invalid chunk size",
+                                ));
+                            }
+                            Ok(status) => {
+                                if status.is_partial() {
+                                    let data = self.stream.fill_buf()?;
+
+                                    if data.is_empty() {
+                                        self.at_eof = true;
+                                        return Ok(0);
+                                    }
+                                    continue;
+                                }
+
+                                break status.unwrap();
+                            }
+                        }
+                    };
+                    self.stream.consume(parsed);
+                    sz = chunk_size as usize;
+                }
+
+                //if it is still zero, it was the last chunk
+                if sz == 0 {
+                    //FIXME:we might want to parse the last \r\n
+                    return Ok(0);
+                }
+
+                let bound = std::cmp::min(sz, buf.len());
+                // leaving two bytes to check for \r\n
+                let internal_bound = std::cmp::min(bound, self.stream.buffer().len() - 2);
+                let written = (&mut buf[..bound]).write(&self.stream.buffer()[..internal_bound])?;
+                self.stream.consume(written);
+                if sz == written {
+                    if &self.stream.buffer()[..2] == &b"\r\n"[..] {
+                        self.stream.consume(2);
+                    } else {
+                        return Err(io::Error::new(io::ErrorKind::Other, "invalid chunk end"));
+                    }
+                }
+
+                (Length::ContentLength(sz - written), Ok(written))
+            }
         };
 
         self.length = length;
@@ -209,7 +262,6 @@ impl<Stream: Read+Debug> Read for Body<Stream> {
         res
     }
 }
-*/
 
 /// used to determie if the body can be sent as is or chunked
 pub trait HasLength {
@@ -237,6 +289,12 @@ pub enum HttpError {
     Http(http::Error),
 }
 
+impl From<ResolverError> for HttpError {
+    fn from(e: ResolverError) -> Self {
+        HttpError::Resolver(e)
+    }
+}
+
 impl From<io::Error> for HttpError {
     fn from(e: io::Error) -> Self {
         HttpError::Io(e)
@@ -261,10 +319,9 @@ pub enum ResolverError {
     ConnectionFailed,
 }
 
-pub trait Resolver<Stream: Read+Write> {
-    fn resolve(hostname: &str) -> Result<Stream, ResolverError>;
+pub trait Resolver<Stream: Read + Write> {
+    fn resolve(url: &str) -> Result<Stream, HttpError>;
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -275,33 +332,44 @@ mod tests {
     struct TcpStreamResolver {}
 
     impl Resolver<TcpStream> for TcpStreamResolver {
-        fn resolve(hostname: &str) -> Result<TcpStream, ResolverError> {
-            println!("resolving hostname: {}", hostname);
-            match hostname.to_socket_addrs() {
+        fn resolve(url: &str) -> Result<TcpStream, HttpError> {
+            let url = url::Url::parse(url).map_err(HttpError::Url)?;
+
+            let host = match url.port_or_known_default() {
+                Some(p) => format!("{}:{}", url.host_str().unwrap(), p),
+                None => url.host_str().unwrap().to_string(),
+            };
+            println!("resolving hostname: {}", host);
+            match host.to_socket_addrs() {
                 Err(e) => {
                     println!("ToSocketAddrs error: {:?}", e);
-                    Err(ResolverError::NotFound)
-                },
+                    Err(ResolverError::NotFound.into())
+                }
                 Ok(mut addr_iter) => match addr_iter.next() {
                     None => {
                         println!("ToSocketAddrs error: no addresses returned");
-                        Err(ResolverError::NotFound)
-                    },
-                    Some(addr) => match TcpStream::connect(addr) {
-                        Err(_) => Err(ResolverError::ConnectionFailed),
-                        Ok(stream) => Ok(stream)
+                        Err(ResolverError::NotFound.into())
                     }
-                }
+                    Some(addr) => match TcpStream::connect(addr) {
+                        Err(_) => Err(ResolverError::ConnectionFailed.into()),
+                        Ok(stream) => Ok(stream),
+                    },
+                },
             }
         }
     }
 
     #[test]
-    fn localhost() {
-        let res = Client::<TcpStream, TcpStreamResolver>::get("http://lolcatho.st:1026/").unwrap();
+    fn clever_cloud() {
+        let mut res =
+            Client::<TcpStream, TcpStreamResolver>::get("http://www.clever-cloud.com/").unwrap();
 
         println!("got response:\n{:?}", res);
-        panic!();
 
+        let mut body = res.body_mut();
+        let mut s = String::new();
+        body.read_to_string(&mut s).unwrap();
+        println!("body:\n{}", s);
+        panic!();
     }
 }
